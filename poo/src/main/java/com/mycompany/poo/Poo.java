9d9dfb40db0b20c913e06400fa117d2f4ad14b6b
Daniel-f56 <dftorradoc@ufpso.edu.co>
2024-03-01 17:31:06 +0100

    Creacion de clase interna

--- poo/src/main/java/com/mycompany/poo/Poo.java
+++ poo/src/main/java/com/mycompany/poo/Poo.java
@@ -1,16 +1,34 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.poo;
 
-/**
- *
- * @author SCIS2PC18
- */
 public class Poo {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Character homero = new Character("Homero", "Inspector de seguridad", 33, "A la grande le puse cuca");
+        homero.greeting();
+        
+        Character bart = new Character ("Bartolomeo", "Vago", 10, "Ay caramba");
+        bart.greeting();
+        
+    }
+    static class Character{
+        String name;
+        String occupation;
+        int age;
+        String phrase;
+
+        public Character(String name, String occupation, int age, String phrase) {
+            this.name = name;
+            this.occupation = occupation;
+            this.age = age;
+            this.phrase = phrase;
+        }
+        
+        void greeting (){
+            System.out.println(this.phrase);
+        }
+        
+        void year (){
+            System.out.println(this.age);
+        }
     }
 }
