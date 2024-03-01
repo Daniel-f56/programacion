718e863a810b9b6090a48929cac82b427523d6c1
Daniel-f56 <dftorradoc@ufpso.edu.co>
2024-03-01 18:03:18 +0100

    Creacion de paquetes, getters y setters con sobrecarga de constructor

--- poo/src/main/java/com/mycompany/poo/Poo.java
+++ poo/src/main/java/com/mycompany/poo/Poo.java
@@ -1,34 +1,17 @@
 package com.mycompany.poo;
 
-public class Poo {
+import characters.Character;
 
-    public static void main(String[] args) {
+
+public class Poo {
+public static void main(String[] args) {
         Character homero = new Character("Homero", "Inspector de seguridad", 33, "A la grande le puse cuca");
         homero.greeting();
+        System.out.println(homero.getAge());
         
-        Character bart = new Character ("Bartolomeo", "Vago", 10, "Ay caramba");
+        Character bart = new Character ();
+        bart.setPhrase("Ay caramba¡¡¡");
         bart.greeting();
         
-    }
-    static class Character{
-        String name;
-        String occupation;
-        int age;
-        String phrase;
-
-        public Character(String name, String occupation, int age, String phrase) {
-            this.name = name;
-            this.occupation = occupation;
-            this.age = age;
-            this.phrase = phrase;
-        }
-        
-        void greeting (){
-            System.out.println(this.phrase);
-        }
-        
-        void year (){
-            System.out.println(this.age);
-        }
     }
 }
